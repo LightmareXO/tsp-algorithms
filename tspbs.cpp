cfccf10b3321030dd6a2b0ff02bb2b429cb9b383
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-13 12:16:27 +0900

    first commit: create template

---
+++ tspbs.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+struct Point {
+    ll x, y;
+};
+
+struct State {
+    Point p;
+    ll score;
+    int LastMove;
+    int LastPos;
+};
+
+bool operator>(const State &a, const State &b) {
+    return a.score > b.score;
+}
+
+ll dist(Point a, Point b) {
+    return round(sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)));
+}
+
+void showNearer(vector<vector<pair<ll, int>>> p) {
+    for (int i = 0; i < p.size(); i++) {
+        for (int j = 0; j < p[i].size(); j++) {
+            cout << p[i][j].first  << " ";
+    }
+        cout << endl;
+    }
+}
+
+int main() {
+    vector<Point> p;
+
+    int id, x, y;
+
+    while (cin >> id >> x >> y) {
+        p.push_back({x, y});
+    }
+
+    int size = p.size();
+
+    // dist, city_id
+    vector<vector<pair<ll, int>>> near(size);
+
+    for (int i = 0; i < size; i++) {
+        vector<pair<ll, int>> tmp;
+
+        for (int j = 0; j < p.size(); j++) {
+            if (i == j) continue;
+
+            tmp.push_back({dist(p[i], p[j]), j});
+            sort(tmp.begin(), tmp.end());
+        }
+
+        near[i] = tmp;
+    }
+
+    showNearer(near);
+
+    const int WIDTH = 10;
+    int NumState[size];
+    State Beam[size][WIDTH];
+    Point schedule[size];
+    auto beamSearch = [&](Point start) -> void {
+        NumState[0] = 1;
+        Beam[0][0].score = 0;
+        Beam[0][0].p = start;
+
+        for (int i = 0; i < size; i++) {
+            vector<State> canditate;
+
+            //ToDo
+
+            sort(canditate.begin(), canditate.end());
+            NumState[i] = min(WIDTH, (int)canditate.size());
+
+            for(int j = 0; j < NumState[i]; j++) {
+                Beam[i][j] = canditate[j];
+            }
+        }
+    };
+
+
+}
