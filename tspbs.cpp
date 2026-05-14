795121ab9319bd2a559198eebc5a4161db710d68
LightmareXO <175393228+LightmareXO@users.noreply.github.com>
2026-05-15 03:35:59 +0900

    TSPにおいてビームサーチを用いた解の探索を実装

--- tspbs.cpp
+++ tspbs.cpp
@@ -1,20 +1,26 @@
+// 注意点: 
+// 距離関数は二点間の距離を四捨五入して使用しているので、
+// 用途に応じてscoreとdist()をdoubleにするかしてください
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+
+const int MAX_N = 200;
 
 struct Point {
     ll x, y;
 };
 
 struct State {
-    Point p; // 現在位置
     ll score; // 暫定スコア
-    int LastMove; // 最後に移動した都市
+    int now; // 今回訪問した都市
     int LastPos; // Beam[i-1][どこ]から遷移したか
+    bitset<MAX_N> visited; // 訪問済み都市の集合(今回含む)
 };
 
-bool operator>(const State &a, const State &b) {
-    return a.score > b.score;
+bool operator<(const State &a, const State &b) {
+    return a.score < b.score;
 }
 
 ll dist(Point a, Point b) {
@@ -39,15 +45,15 @@ int main() {
         p.push_back({x, y});
     }
 
-    int size = p.size();
+    int CITIES = p.size();
 
     // dist, city_id
-    vector<vector<pair<ll, int>>> near(size);
+    vector<vector<pair<ll, int>>> near(CITIES);
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < CITIES; i++) {
         vector<pair<ll, int>> tmp;
 
-        for (int j = 0; j < p.size(); j++) {
+        for (int j = 0; j < CITIES; j++) {
             if (i == j) continue;
 
             tmp.push_back({dist(p[i], p[j]), j});
@@ -60,32 +66,70 @@ int main() {
     // showNearer(near);
 
     const int WIDTH = 10;
-    int NumState[size];
-    State Beam[size][WIDTH];
-    Point schedule[size];
+    vector<int> NumState(CITIES+1);
+    vector<vector<State>> Beam(CITIES, vector<State>(WIDTH));
 
-    auto beamSearch = [&](Point start) -> void {
+    auto beamSearch = [&](int start) -> void {
         NumState[0] = 1;
         Beam[0][0].score = 0;
-        Beam[0][0].p = start;
+        Beam[0][0].now = start;
+        Beam[0][0].visited.reset();
+        Beam[0][0].visited.set(start);
 
-        for (int i = 1; i <= size; ++i) {
+        // i個めの都市に訪れる
+        for (int i = 1; i < CITIES; ++i) {
             vector<State> candidate;
 
-            // 
+            // 各状態について
             for (int j = 0; j < NumState[i-1]; ++j) {
-                
+                // 次に行く都市を探索
+                for (int k = 0; k < CITIES; ++k) {
+                    State nowState = Beam[i-1][j];
+                    // 訪問済みならスキップ
+                    if (nowState.visited.test(k)) continue;
+                    
+                    nowState.now = k;
+                    nowState.LastPos = j;
+                    nowState.visited.set(k);
+                    nowState.score += dist(p[Beam[i-1][j].now], p[k]);
+
+                    candidate.emplace_back(nowState);
+                }
             }
 
-
+            // 候補のtop WIDTH個をとってくる
             sort(candidate.begin(), candidate.end());
             NumState[i] = min(WIDTH, (int)candidate.size());
-
             for(int j = 0; j < NumState[i]; j++) {
                 Beam[i][j] = candidate[j];
             }
         }
     };
 
+    auto restorePath = [&]() -> vector<int> {
+        vector<int> path(CITIES);
+        int curr = 0;
+        for (int i = CITIES-1; i >= 0; --i) {
+            path[i] = Beam[i][curr].now;
+            if (i > 0) {
+                curr = Beam[i][curr].LastPos;
+            }
+        }
+        return path;
+    };
 
+    ll bestScore = LLONG_MAX;
+    vector<int> bestSchedule;
+    // 各頂点スタートを試す
+    for (int start = 0; start < CITIES; ++start) {
+        fill(NumState.begin(), NumState.end(), 0);
+        beamSearch(start);
+        ll totalScore = Beam[CITIES-1][0].score + dist(p[Beam[CITIES-1][0].now], p[start]);
+        if (bestScore > totalScore) {
+            bestScore = totalScore;
+            bestSchedule = restorePath();
+        }
+    }
+
+    cout << bestScore << endl;
 }
